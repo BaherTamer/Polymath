ed030627dda5a03692ba59d7ce328c43271b11f8
Baher Tamer <baher.appledev@gmail.com>
2023-08-24 18:04:24 +0300

    Setup main tab bar controller
    - Add three tab items (following - search - downloaded)
    - Embed each view controller to navigation controller

---
+++ Polymath/Polymath/Controllers/MainTabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  MainTabBarController.swift
+//  Polymath
+//
+//  Created by Baher Tamer on 24/08/2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().prefersLargeTitles = true
+        
+        setupTabBarViewControllers()
+    }
+    
+}
+
+// MARK: - Setup Functions
+extension MainTabBarController {
+    
+    private func setupTabBarViewControllers() {
+        
+        tabBar.tintColor = .systemPink
+        
+        viewControllers = [
+            setupNavigationController(for: ViewController(), title: "Following", imageName: "headphones.circle"),
+            setupNavigationController(for: ViewController(), title: "Search", imageName: "magnifyingglass.circle"),
+            setupNavigationController(for: ViewController(), title: "Downloaded", imageName: "arrow.down.circle")
+        ]
+    }
+    
+    private func setupNavigationController(for rootViewController: UIViewController, title: String, imageName: String) -> UINavigationController {
+        
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.tabBarItem.title = title
+        navigationController.tabBarItem.image = UIImage(systemName: imageName)
+        navigationController.tabBarItem.selectedImage = UIImage(systemName: "\(imageName).fill")
+        
+        rootViewController.navigationItem.title = title
+        
+        return navigationController
+    }
+    
+}
