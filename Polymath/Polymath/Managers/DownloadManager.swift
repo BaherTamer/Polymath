1d1331064f49f8cf0dd6ed81e4c33c05388f1c36
Baher Tamer <baher.appledev@gmail.com>
2023-09-02 14:36:20 +0300

    Create downloading episodes logic manager

---
+++ Polymath/Polymath/Managers/DownloadManager.swift
@@ -0,0 +1,73 @@
+//
+//  DownloadManager.swift
+//  Polymath
+//
+//  Created by Baher Tamer on 02/09/2023.
+//
+
+import Foundation
+
+struct DownloadManager {
+    
+    static private let downloadingEpisodeKey = "downloadingEpisodeKey"
+    static private var episodes: [Episode] = []
+    
+    static func fetchDownloadedEpisodes() -> [Episode] {
+        do {
+            if let data = UserDefaults.standard.data(forKey: Self.downloadingEpisodeKey) {
+                return try JSONDecoder().decode([Episode].self, from: data)
+            }
+        } catch {
+            print("DEBUG: Failed to fetch downloaded episodes,", error)
+            return []
+        }
+        
+        return []
+    }
+    
+    static func downloadEpisode(_ episode: Episode) {
+        Self.episodes = Self.fetchDownloadedEpisodes()
+        
+        do {
+            Self.episodes.insert(episode, at: 0)
+            try Self.saveDownloadedEpisodes()
+        } catch {
+            print("DEBUG: Failed to save downloaded episodes,", error)
+        }
+    }
+    
+    static func isEpisodeDownloaded(_ episode: Episode) -> Bool {
+        Self.episodes = Self.fetchDownloadedEpisodes()
+        return Self.episodes.contains(where: { $0.title == episode.title })
+    }
+    
+    static func saveDownloadedEpisodes() throws {
+        do {
+            let data = try JSONEncoder().encode(Self.episodes)
+            UserDefaults.standard.set(data, forKey: Self.downloadingEpisodeKey)
+        } catch {
+            throw error
+        }
+    }
+    
+    static func removeDownloadedEpisode(_ episode: Episode) {
+        guard Self.isEpisodeDownloaded(episode) else {
+            print("DEBUG: Episode was not found")
+            return
+        }
+        
+        guard let index = Self.episodes.firstIndex(where: { $0.title == episode.title }) else {
+            print("DEBUG: Index was not found")
+            return
+        }
+        
+        Self.episodes.remove(at: index)
+        
+        do {
+            try Self.saveDownloadedEpisodes()
+        } catch {
+            print("DEBUG: Failed to remove downloaded episodes,", error)
+        }
+    }
+    
+}
